b68f83a58ca0b850685d9449810a3b59f6fa8131
cesar.cruz.shift <cesar.cruz@shift.com.br>
2022-05-08 02:53:03 -0300

    feat: viewer kudos

--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
@@ -11,7 +10,7 @@ import { HomeUiModule } from '@kudostories/home/ui';
 
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSliderModule, HomeUiModule, SharedUiModule],
   providers: [],
   bootstrap: [AppComponent],
